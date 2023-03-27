264fbb9f140377b1743f27055cc91dfe643e2ad5
internetdrew <andrew@internetdrew.com>
2023-03-27 14:18:39 -0400

    play with simple grid ui component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,34 +1,24 @@
-import { Box, Container, Flex, Heading, Text } from '@chakra-ui/react';
+import { Box, SimpleGrid } from '@chakra-ui/react';
 import React from 'react';
 
 const Dashboard = () => {
-  const boxStyles = {
-    p: '10px',
-    bg: 'purple.400',
-    color: 'white',
-    m: '10px',
-    textAlign: 'center',
-    filter: 'blur(2px)',
-    ':hover': {
-      color: 'black',
-      bg: 'blue',
-    },
-  };
-
   return (
-    <Container as='section' maxW={''} py='20px'>
-      <Flex justify={'space-between'} alignItems='center'>
-        <Heading>Chakra UI Components</Heading>
-        <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
-          quis.
-        </Text>
-      </Flex>
-      <Box p={4} borderWidth='1px' w='80%'>
-        <Text>This is something</Text>
-      </Box>
-      <Box sx={boxStyles}>hello ninjas</Box>
-    </Container>
+    <SimpleGrid p='10px' spacing={10} minChildWidth='250px'>
+      <Box bg='white' h='200px' border={'1px solid'}></Box>
+      <Box bg='white' h='200px' border={'1px solid'}></Box>
+      <Box bg='white' h='200px' border={'1px solid'}></Box>
+      <Box bg='white' h='200px' border={'1px solid'}></Box>
+
+      <Box bg='white' h='200px' border={'1px solid'}></Box>
+      <Box bg='white' h='200px' border={'1px solid'}></Box>
+      <Box bg='white' h='200px' border={'1px solid'}></Box>
+      <Box bg='white' h='200px' border={'1px solid'}></Box>
+
+      <Box bg='white' h='200px' border={'1px solid'}></Box>
+      <Box bg='white' h='200px' border={'1px solid'}></Box>
+      <Box bg='white' h='200px' border={'1px solid'}></Box>
+      <Box bg='white' h='200px' border={'1px solid'}></Box>
+    </SimpleGrid>
   );
 };
 
