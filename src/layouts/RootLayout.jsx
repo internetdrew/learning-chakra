383304dd7be5b392a737261761a69dee20adae07
internetdrew <andrew@internetdrew.com>
2023-03-27 14:39:36 -0400

    test out defining grid layouts

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,13 +1,19 @@
+import { Grid, GridItem } from '@chakra-ui/react';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 
 const RootLayout = () => {
   return (
-    <div>
-      <Navbar />
-      <Outlet />
-    </div>
+    <Grid templateColumns={'repeat(6, 1fr)'} bg='gray.50'>
+      <GridItem as={'aside'} colSpan='1' bg={'purple.400'} minH='100vh' p='30'>
+        <span>Sidebar</span>
+      </GridItem>
+      <GridItem as='main' colSpan={'5'}>
+        <Navbar />
+        <Outlet />
+      </GridItem>
+    </Grid>
   );
 };
 
