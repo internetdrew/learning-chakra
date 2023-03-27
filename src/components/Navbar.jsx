d9ad4bf4f321efaad2cb04afd4d8cf3e0f0dc995
internetdrew <andrew@internetdrew.com>
2023-03-27 14:11:52 -0400

    add navbar to dashboard

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,29 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  Spacer,
+  Text,
+} from '@chakra-ui/react';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Flex as={'nav'} p='10px' alignItems={'center'} gap='10px'>
+      <Heading as={'h1'}>Dojo Tasks</Heading>
+      <Spacer />
+
+      <HStack spacing={'20px'}>
+        <Box bg='gray.200' p='10px'>
+          M
+        </Box>
+        <Text>mario@gmail.com</Text>
+        <Button colorScheme='purple'>Logout</Button>
+      </HStack>
+    </Flex>
+    //
+  );
 };
 
 export default Navbar;
