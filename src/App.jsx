608452f4125dff9de9ebd085459eda9412cba28e
internetdrew <andrew@internetdrew.com>
2023-03-27 12:25:45 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>hello world</h1>
+    </>
+  );
+}
+
+export default App;
